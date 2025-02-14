2d01246b3b3396a662cb4ba02a61c1a19e5ec790
liveintottenham <immin.0113@gmail.com>
2025-02-14 20:05:27 +0900

    250215_kakao7

--- main.py
+++ main.py
@@ -477,7 +477,7 @@ def branch_info_page():
                         # 수정된 HTML/JS 코드
                         map_html = f"""
                         <div id="map" style="width:95%;height:400px;border-radius:12px;margin:0 auto;"></div>
-                        <script type="text/javascript" src="https://dapi.kakao.com/v2/maps/sdk.js?appkey={kakao_api_key}"></script>
+                        <script type="text/javascript" src="https://dapi.kakao.com/v2/maps/sdk.js?appkey={kakao_api_key}&libraries=services"></script>
                         <script>
                             // 카카오 지도 API 로드 완료 후 초기화
                             kakao.maps.load(function() {{
